25c57570fc117ee8e6448c036f71a3bb99f38abc
Hunter Morgan <hmorgan@heyo.com>
2014-07-08 19:28:13 -0400

    0.0.3 is mainly documentation/organization/administrative
    Also, it was verified to work, at least in the example case, which is
    valuable.

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
-madge-targeted
-==============
+#madge-targeted
+##Description
+This project uses MaDGe to get specific amd/cjs dependency trees from big
+codebases.
 
-targeted madge to get amd/cjs dependency tree from big codebases
+###Notes
+This is a work in progress currently. There is command-line help. Documentation
+is a new thing while this goes from a quick hack to a useful product. More to
+come.
+
+Also, this may be broken at any time, even the very head you're looking at right
+now.
+
+##Usage
+madge-targeted presently takes a directory subtree of modules and an entry point
+for the code, and builds a dependency tree from the entry point, graphing all
+modules that depend on it. This generates a top-down view of an application or
+module.
+
+###Examples
+```
+user@host:~/git/madge-targeted/ > ./madge-targeted.js -e heyo225/main \
+-d ../heyo-resources/src/js/dojo/
+```
+prints a dot file to stdout containing a digraph of all live code that the app's
+main entry point explicitly uses. (Excludes dynamically loaded editor widgets, I
+think.)
+
+##Design
+The processing of MaDGe output is pretty naive, but originated in a proof of
+concept. There's plenty of room for improvement.
+
+##Goals
+The next two priority goals (after potentially fixing if broken) are to:
+* facilitate batch processing of codebases
+* support reverse traversal to get consumers of target modules
+
+###Misc. Todo
+More stuff that maybe needs done:
+* potentially refactor stuff
+* potentially async some things
+* support graphviz rendering
+* put issues in GitHub tracker
+* write tests
+
+Maybe nice to haves:
+* build fancy-pants html-ized moving graphs with dependo
+* maybe make api?
+
+Btw, 1.0 will be called Beefy Miracle, just for Bryan Lunduke.
+<!-- ##API -->
--- madge-targeted.js
+++ madge-targeted.js
@@ -1,33 +1,59 @@
 #!/usr/bin/env node
+/**
+ * @file uses MaDGe to generate a dependency tree and then processes that output
+ * to drill down in a larger codebase and visualize specific subtrees.
+ *
+ * @see        {@link https://github.com/automaticgiant/madge-targeted | GitHub}
+ * @see        {@link https://github.com/pahen/madge | MaDGe GitHub}
+ * @version    0.0.3
+ * @since      2014-07-07
+ * @author     Hunter Morgan<automaticgiant@gmail.com>
+ * @requires   commander
+ * @requires   madge
+ * @requires   graphviz
+ * @requires   fs
+ */
 
+
+/**
+ * Array of dependency modules to require
+ * @type {Array}
+ */
 var myDeps = [
-  'commander',
-  'madge',
+  'commander', //commandline options
+  'madge', //dependency tree building
   'graphviz',
-  'fs'
-];
+  'fs' //node fs stuff
+]; // then eval require them
 for (dep in myDeps) eval('var '+myDeps[dep]+"=require('"+myDeps[dep]+"');");
 
+
+//commandline options, by configuration, should be sufficiently documented
 commander
-  .version('0.0.2')
+  .version('0.0.3')
   .option('-e, --entry_point <module name>', 'Set parent of dependency tree')
   .option('-d, --src_dir <directory(s)>',
     'Set directory(s) containing source modules')
   .option('-f, --module_format [name]', 'format to parse (amd/cjs, amd default')
   .option('-x, --exclude <regex>', 'a regular expression for excluding modules')
   .option('-o, --outfile [name]', 'a file to print dot graph to')
+  .option('-v, --verbose', 'print extra messages')
+  .option('-t, --trace', 'trace traversal')
   // .option('-i, --image [name]', 'a file to write graph image to')
   // .option('-l, --layout [engine]', 'layout engine to use for rendering image')
   //add arg validator, esp engine
   .parse(process.argv);
 
+//required options
 if (!commander.src_dir||!commander.entry_point) {
-  // console.error(commander);
+  console.error(commander, "Sorry. Commander's free help option doesn't seem " +
+    "to be working.");
   console.error('Use your words (--help to get a list)');
   process.exit(1);
 }
 
-console.error('parsing');
+commander.verbose && console.error('parsing');
+//have MaDGe do the hard work
 var depTree = madge(commander.src_dir,
   {
     format: commander.module_format||'amd',
@@ -40,11 +66,12 @@ queue.push(entryPoint);
 var graph = graphviz.digraph('"' + entryPoint + ' deps"');
 do {
   var node = queue.pop();
-  console.error('Walking ' + node + '. Queue length: ' + queue.length);
+  commander.trace && console.error('Walking ' + node + '. Queue length: ' +
+    queue.length);
   graph.addNode(node);
   for (var index in depTree[node]){
     var target = depTree[node][index];
-    // console.error('queueing/graphing ' + target + '...');
+    commander.trace && console.error('queueing/graphing ' + target + '...');
     queue.push(target);
     graph.addNode(target);
     graph.addEdge( node, target );
@@ -52,14 +79,19 @@ do {
   delete queue[node];
 } while (queue.length!==0);
 
+//the simple traversal potentially generated duplicates
+
 var dot = graph.to_dot().split('\n');
 
+//stole this dedup code from StackExchange
 uniqDot = dot.filter(function(elem, pos) {
     return dot.indexOf(elem) == pos;
 });
 
 var output = uniqDot.join('\n');
 
+//this is more complicated than it needs to be maybe, but I thought I could
+//already do image output easily
 if (commander.outfile) 
   // if (commander.image) 
   // }
